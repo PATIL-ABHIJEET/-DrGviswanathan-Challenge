df540038cfbddba668ef74fc9c6f4b225ea94d81
PATIL-ABHIJEET <abhijeetpatil24922@gmail.com>
2026-06-09 18:14:06 +0530

    DAY5

---
+++ DAY5/Solution.java
@@ -0,0 +1,33 @@
+package DAY5;
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+            
+        }
+        int[] result = new int[k];
+
+        for (int i = 0; i < k; i++) {
+
+            int maxFreq = 0;
+            int element = 0;
+
+            for (int key : map.keySet()) {
+
+                if (map.get(key) > maxFreq) {
+                    maxFreq = map.get(key);
+                    element = key;
+                }
+            }
+
+            result[i] = element;
+
+            map.remove(element);
+        }    
+
+            return result;
+
+    }
+}
